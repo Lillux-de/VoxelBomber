722414db212e6ddde0329e31b91f3a95872a7a81
Pfollvosten <lukas.boehm00@gmx.de>
2018-07-14 17:23:01 +0200

    Aufraeumen

--- VoxelBomber/Assets/Scripts/InputController.cs
+++ VoxelBomber/Assets/Scripts/InputController.cs
@@ -2,16 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementController : MonoBehaviour {
+public class InputController : MonoBehaviour {
 
-	private Animator anim;
 	// Use this for initialization
 	void Start () {
-		anim = GetComponent<Animator>();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		anim.SetFloat("speed", Input.GetAxis("Vertical"));
+		
 	}
 }
